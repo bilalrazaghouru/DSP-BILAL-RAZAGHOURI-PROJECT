5acbf1c33d74f81b4307ef3c532c7caa83066a9e
Muhammad Ahmad SHAMOON <juttahmad363@gmail.com>
2025-10-11 16:06:34 +0500

    Created webapp using streamlit

--- api/create_tables.py
+++
@@ -1,6 +0,0 @@
-from database import Base, engine
-import models  # ensure SQLAlchemy models are registered
-
-if __name__ == "__main__":
-    Base.metadata.create_all(bind=engine)
-    print("✅ Tables created.")
